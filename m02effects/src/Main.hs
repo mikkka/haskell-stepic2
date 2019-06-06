2ed9f29ea87b4ac3328f37d63721b902b12be1f7
m.tkachev <m.tkachev@tinkoff.ru>
2019-06-06 20:40:46 +0500

    theory solution

--- m02effects/src/Main.hs
+++ m02effects/src/Main.hs
@@ -2,3 +2,20 @@ module Main where
 
 main :: IO ()
 main = putStrLn "hello world"
+
+
+
+{-
+sequenceA :: Applicative f => t (f a) -> f (t a)
+
+(Applicative f, Applicative g, Traversable t)
+
+t (f (g a)) -> Compose f g (t a)
+
+Compose . fmap sequenceA . sequenceA
+
+fmap sequenceA . sequenceA == t (f (g a)) -> f g (t a) 
+
+
+sequenceA == t (f (g a)) -> f (t (g a)) 
+-}
