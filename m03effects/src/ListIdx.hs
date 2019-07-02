3805b980e3e8d49132da2c3659ab4ec1e1f1784e
Mikhail Tkachev <mick@MacBook-Pro-Mikhail.local>
2019-07-02 23:30:43 +0500

    3.1 end solutions

--- m03effects/src/ListIdx.hs
+++ m03effects/src/ListIdx.hs
@@ -1,6 +1,7 @@
 module ListIdx where
 
 import Control.Monad.Trans.Except
+import Data.Foldable
 
 data ListIndexError = ErrIndexTooLarge Int | ErrNegativeIndex 
   deriving (Eq, Show)
@@ -13,3 +14,22 @@ data ListIndexError = ErrIndexTooLarge Int | ErrNegativeIndex
       scan xs idx = case xs of 
         []       -> except $ Left $ (ErrIndexTooLarge index)
         (x:tail) -> if idx == 0 then except $ Right x else scan tail (idx - 1)  
+
+
+newtype SimpleError = Simple { getSimple :: String } 
+  deriving (Eq, Show)
+      
+instance Semigroup SimpleError where 
+  (<>) s1 s2 = s1 `mappend` s2
+
+instance Monoid SimpleError where
+  mempty = Simple ""
+  mappend (Simple s1) (Simple s2) = Simple $ s1 ++ s2
+
+lie2se :: ListIndexError -> SimpleError
+lie2se ErrNegativeIndex     = Simple "[negative index]"
+lie2se (ErrIndexTooLarge i) = Simple $ "[index (" ++ show i ++ ") is too large]"
+  
+toSimple = runExcept . withExcept lie2se
+toSimpleFromList = runExcept . msum . map (withExcept lie2se)
+
--- m03effects/src/SumExcept.hs
+++ m03effects/src/SumExcept.hs
@@ -18,3 +18,21 @@ trySum strs = sum strs 1
 
 trySum2 :: [String] -> Except SumError Integer
 trySum2 xs = sum <$> traverse (\(i, s) -> withExcept (SumError i) $ tryRead s) (zip [1..] xs)
+
+
+newtype Validate e a = Validate {getValidate :: Either [e] a}
+instance Functor (Validate e) where
+  fmap f x = pure f <*> x
+
+instance Applicative (Validate e) where
+  pure = Validate . Right
+  (<*>) (Validate (Left e1)) (Validate (Left e2)) = Validate $ Left (e1 ++ e2)
+  (<*>) (Validate (Right _)) (Validate (Left e2)) = Validate $ Left e2
+  (<*>) (Validate (Left e1)) (Validate (Right _)) = Validate $ Left e1
+  (<*>) (Validate (Right f)) (Validate (Right x)) = Validate $ Right (f x)
+
+collectE :: Except e a -> Validate e a
+collectE = Validate . either (\a -> Left [a]) Right . runExcept
+
+validateSum :: [String] -> Validate SumError Integer
+validateSum xs = sum <$> traverse (\(i, s) -> collectE (withExcept (SumError i) $ tryRead s)) (zip [1..] xs)
